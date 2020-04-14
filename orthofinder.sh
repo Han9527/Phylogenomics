ed84b9b1dc915fa43c5395de61f72654ebea41e8
Han Zhigang <hanzg@zju.edu.cn>
2020-04-14 16:59:45 +0800

    orthofinder script

---
+++ orthofinder.sh
@@ -0,0 +1,8 @@
+date
+orthofinder -f /home/sda1/workspace/MetalRNA/18.comparative_genome/01.aa/14.only_barley/00.raw_data/ \
+     -S diamond \
+     -M msa \
+     -T fasttree \
+     -t 60 \
+#sleep 2s
+date
